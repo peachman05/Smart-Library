80ba54d37905b46cba8c7f96ad05aca910d3abf9
isara <isaradream@gmail.com>
2016-10-09 05:08:17 +0700

    Create Dir for templates and static files. And create sample index page (Using template and static) :)

--- smart_library/settings.py
+++ smart_library/settings.py
@@ -70,18 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'smart_library.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -123,7 +111,34 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'library/static'),
 )
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            'templates',
+            'library/templates'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # For Heroku
 import dj_database_url
@@ -134,6 +149,20 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+    # in local_settings.py
+        # DATABASES = {
+        #     'default': {
+        #         'ENGINE': 'django.db.backends.postgresql',
+        #         'NAME': 'mydatabase',
+        #         'USER': 'mydatabaseuser',
+        #         'PASSWORD': 'mypassword',
+        #         'HOST': '127.0.0.1',
+        #         'PORT': '5432',
+        #     }
+        # }
 
 try:
     from .local_settings import *
