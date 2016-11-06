04b8b8a6e89557d887eeb35dcaeeccdaa31cefc5
isara <isaradream@gmail.com>
2016-11-06 23:52:56 +0700

    Modify pao's fontend to template

--- library/catalog_views.py
+++ library/catalog_views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>this is catalog view homepage</h2>")
+    return render(request, 'catalog.html', {})
 
 def detail(request,book_id):
     return HttpResponse("<h3>Detail for book id : "+book_id +"</h3>")
