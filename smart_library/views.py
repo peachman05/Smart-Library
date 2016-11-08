366a6cf9df128f72f48b3e9f678ac6604d3cfedb
ASUS-K501LX\sirid <siridaj.pao@gmail.com>
2016-11-08 08:28:26 +0700

    Add forget path that is missing

--- smart_library/views.py
+++ smart_library/views.py
@@ -39,3 +39,32 @@ def login(request):
 def logout(request):
     auth_logout(request)
     return HttpResponseRedirect('/login/')
+def forgetpass(request):
+    data = {}
+    if request.method == 'POST':
+        studentid = request.POST['studentid']
+        email = request.POST['email']
+        try:
+            studentx = Student.objects.get(student_ID = studentid)
+        except:
+            data['error_message'] = 'Student ID Not Found'
+            return render(request, "forgetp.html", {})
+        if(studentx.user.email != email):
+            data['error_message'] = 'Student ID and email isn\'t match'
+            return render(request, "forgetp.html", {})
+        else:
+            passwd = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(9))
+            mail_message = 'Dear '+studentx.user.first_name+' '+studentx.user.last_name
+            mail_message += '\n\n\n\t Your password is reset!!\n\t So your new password is: '+passwd
+            mail_message += '\n\n\nThank, \nSmart-Library Teams.'
+            send_mail(
+                'Your Account\'s password is reset!',
+                mail_message,
+                settings.EMAIL_HOST_USER,
+                [email],
+                fail_silently=True,
+            )
+            studentx.user.set_password(passwd)
+            studentx.user.save()
+            return HttpResponseRedirect('/')
+    return render(request, "forgetp.html", {})
