f15c805243111d50a95cb00b29c0716793475bab
ASUS-K501LX\sirid <siridaj.pao@gmail.com>
2016-11-08 08:12:14 +0700

    Check if SuperUser then redirect to librarian's portal

--- smart_library/views.py
+++ smart_library/views.py
@@ -3,12 +3,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
 from django.contrib.auth import authenticate, logout as auth_logout, login as auth_login
-from library.models import Student
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
-import random
-import string
-from django.conf import settings
 
 def home(request):
 	return HttpResponseRedirect('/lib/')
@@ -26,7 +20,10 @@ def login(request):
             if user.is_active:
                 auth_login(request, user)
                 # Done!
-                return HttpResponseRedirect('/')
+                if user.is_superuser:
+                    return HttpResponseRedirect('/lib/librarian/')
+                else:
+                    return HttpResponseRedirect('/')
             else:
                 # Account if disabled
                 data['error_message'] = "Your account is disabled"
@@ -38,38 +35,7 @@ def login(request):
     else:
         return render(request, "login.html", {})
 
-@login_required(login_url='/')
+@login_required(login_url='/login/')
 def logout(request):
     auth_logout(request)
-    return HttpResponseRedirect('/')
-
-def forgetpass(request):
-    data = {}
-    if request.method == 'POST':
-        studentid = request.POST['studentid']
-        email = request.POST['email']
-        try:
-            studentx = Student.objects.get(student_ID = studentid)
-        except:
-            data['error_message'] = 'Student ID Not Found'
-            return render(request, "forgetp.html", {})
-        if(studentx.user.email != email):
-            data['error_message'] = 'Student ID and email isn\'t match'
-            return render(request, "forgetp.html", {})
-        else:
-            print('OK5')
-            passwd = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(9))
-            mail_message = 'Dear '+studentx.user.first_name+' '+studentx.user.last_name
-            mail_message += '\n\n\n\t Your password is reset!!\n\t So your new password is: '+passwd
-            mail_message += '\n\n\nThank, \nSmart-Library Teams.'
-            send_mail(
-                'Your Account\'s password is reset!',
-                mail_message,
-                settings.EMAIL_HOST_USER,
-                [email],
-                fail_silently=True,
-            )
-            studentx.user.set_password(passwd)
-            studentx.user.save()
-            return HttpResponseRedirect('/')
-    return render(request, "forgetp.html", {})
+    return HttpResponseRedirect('/login/')
